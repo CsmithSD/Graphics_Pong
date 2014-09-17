2fa7a5ceeb2afefce06c4d9552f8ea630b880511
Carlson <1900227@linux04.mcs.sdsmt.edu>
2014-09-16 20:01:44 -0600

    Added Circle and Rectangle Header

---
+++ include/Rectangle.h
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * File: Rectangle.h
+ * Authors: Ian Carlson, Christopher Smith
+ * Date: 09/16/14
+ * Last Modified: 09/16/14
+ * Description: Header file for the Rectangle class.
+ * Inherits: Entity2D.h
+ * Inherited by: 
+ * ***************************************************************************/
+
+#include "Color.h"
+#include "Entity2D.h"
+
+class Rectangle : public Entity2D.h
+{
+    Rectangle(float width = 0, float height = 0, float x = 0, float y = 0);
+    ~Rectangle();
+    
+    void draw();
+    void set_color(float r = 0; float g = 0; float b = 0);
+    void set_width(float width);
+    void set_height(float height);
+    void set_size(float width, float height);
+
+    private:
+    float width;
+    float height;
+    Color color;
+};
