0538a19a496187df0fb76569572816daf04981b1
LostOracle <carlson_ian@hotmail.com>
2014-09-16 20:11:35 -0600

    test commit 2

---
+++ include/Color.h
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * File: Color.h
+ * Authors: Ian Carlson, Christopher Smith
+ * Date: 09/16/14
+ * Last Modified: 09/16/14
+ * Description: Simple struct definition for handling colors. 
+ * ***************************************************************************/
+
+struct Color
+{
+    float r;
+    float g;
+    float b;
+};
