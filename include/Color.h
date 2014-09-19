84bc80d7e7b1ed5e1979b6414718b4472ed1e520
LostOracle <carlson_ian@hotmail.com>
2014-09-19 11:00:51 -0600

    Fixed Color.h to be only a header file

--- include/Color.h
+++ include/Color.h
@@ -10,8 +10,7 @@
 #define _COLOR_H_
 struct Color
 {
-    Color(float r = 1, float g = 1, float b = 1)
-        :r(r), g(g), b(b){};
+    Color(float r = 1, float g = 1, float b = 1);
     float r;
     float g;
     float b;
