7dac42512cee654b434cfb8cd954b73818c6beda
CsmithSD <Chris.SDSMT@gmail.com>
2014-09-19 11:38:42 -0600

    Changed a lot

--- src/OpenGLDemo.cpp
+++ src/OpenGLDemo.cpp
@@ -1,41 +1,21 @@
-/*
-                    ***** OpenGLDemo.cpp*****
+//Class include files
+#include "../include/Rectangle.h"
+#include "../include/Circle.h"
+#include "../include/Entity2D.h"
 
-OpenGL graphics program, illustrating use of GLUT and callback functions.
-
-CSC433/533 Computer Graphics - Fall 2014
-
-Author: John M. Weiss, Ph.D.
-Originally written January 20, 2006.
-
-Modifications:
-*/
-
-// include files
+// include files c/c++ libraries
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // the GLUT header automatically includes gl.h and glu.h
 #include <GL/freeglut.h>
 
-// symbolic constants
-// define PI for stupid Windows compilers that do not supply it
-#ifndef M_PI
-const float M_PI = 3.14159265358979323846;
-#endif
-
 // keypresses
 const int EscapeKey = 27;
 
-// colors
-const float Black[]     = { 0.0, 0.0, 0.0 };
-const float Red[]       = { 1.0, 0.0, 0.0 };
-const float Green[]     = { 0.0, 1.0, 0.0 };
-const float Blue[]      = { 0.0, 0.0, 1.0 };
-const float Magenta[]   = { 1.0, 0.0, 1.0 };
-const float Cyan[]      = { 0.0, 1.0, 1.0 };
-const float Yellow[]    = { 1.0, 1.0, 0.0 };
-const float White[]     = { 1.0, 1.0, 1.0 };
+//global list of Drawable objects
+vector<Entity2D> Entity_List;
 
 // world coordinate window extents: -1000 to +1000 in smaller dimension
 const float ViewplaneSize = 1000.0;
@@ -53,19 +33,19 @@ void mousedrag( int x, int y );
 
 // useful graphics function prototypes
 void initOpenGL( void );
-void DrawLine( float x1, float y1, float x2, float y2, const float color[] );
-void DrawRectangle( float x1, float y1, float x2, float y2, const float color[] );
-void DrawFilledRectangle( float x1, float y1, float x2, float y2, const float color[] );
-void DrawEllipse( float xRadius, float yRadius, int x, int y, const float color[] );
-void DrawFilledEllipse( float xRadius, float yRadius, int x, int y, const float color[] );
-void DrawBitmapString( const char *string, float x, float y, const float color[] );
-void DrawStrokeString( const char *string, float x, float y, const float color[] );
 
 /******************************************************************************/
 
 // main() function
 int main( int argc, char *argv[] )
 {
+
+    Rectangle my_rect( 10, 10, 50, 50 );
+    Circle my_circ( 30, 10, 10 );
+
+    Entity_List.push_back( my_rect );
+    Entity_List.push_back( my_circ );
+
     // perform various OpenGL initializations
     glutInit( &argc, argv );
     initOpenGL();
@@ -107,17 +87,9 @@ void display( void )
     // clear the display
     glClear( GL_COLOR_BUFFER_BIT );
 
-    // draw a line, rectangles, and ellipses
-    // note that objects are resized with window
-    DrawLine( -ViewplaneSize + 10, -ViewplaneSize + 20, ViewplaneSize - 10, ViewplaneSize - 20, Yellow );
-    DrawRectangle( 300, 200, 900, 400, Cyan );
-    DrawFilledRectangle( -700, -100, -400, -800, Red );
-    DrawEllipse( 300, 200, 600, -300, Green );
-    DrawFilledEllipse( 300, 200, -600, 300, Magenta );
 
-    // label display with text
-    DrawStrokeString( "OpenGL Demo (stroke)", -0.9 * ViewplaneSize, 0.8 * ViewplaneSize, White );
-    DrawBitmapString( "OpenGL Demo (bitmap)", -0.1 * ViewplaneSize, -0.8 * ViewplaneSize, White );
+    for( auto i : Entity_List )
+        i.draw();
 
     // flush graphical output
     glFlush();
@@ -193,114 +165,3 @@ void mouseclick( int button, int state, int x, int y )
             break;
     }
 }
-
-/******************************************************************************/
-/*                          useful graphics routines                          */
-/******************************************************************************/
-
-// draw a line using the given RGB color value
-void DrawLine( float x1, float y1, float x2, float y2, const float color[] )
-{
-    glColor3fv( color );
-    glBegin( GL_LINES );
-        glVertex2f( x1, y1 );
-        glVertex2f( x2, y2 );
-    glEnd();
-    glFlush();
-}
-
-/******************************************************************************/
-
-// draw a rectangle using the given RGB color value
-void DrawRectangle( float x1, float y1, float x2, float y2, const float color[] )
-{
-    glColor3fv( color );
-    glBegin( GL_LINE_LOOP );
-        glVertex2f( x1, y1 );
-        glVertex2f( x2, y1 );
-        glVertex2f( x2, y2 );
-        glVertex2f( x1, y2 );
-    glEnd();
-    glFlush();
-}
-
-/******************************************************************************/
-
-// draw a filled rectangle using the given RGB color value
-void DrawFilledRectangle( float x1, float y1, float x2, float y2, const float color[] )
-{
-    glColor3fv( color );
-    glRectf( x1, y1, x2, y2 );
-    /* or draw a polygon:
-    glBegin( GL_POLYGON );
-        glVertex2f( x1, y1 );
-        glVertex2f( x2, y1 );
-        glVertex2f( x2, y2 );
-        glVertex2f( x1, y2 );
-    glEnd();
-    */
-    glFlush();
-}
-
-/******************************************************************************/
-
-// draw an ellipse of given radii at a specified position using the given RGB color value
-void DrawEllipse( float xRadius, float yRadius, int x, int y, const float color[] )
-{
-    float radius = xRadius < yRadius ? xRadius : yRadius;   // stretch circle into ellipse
-    glColor3fv( color );
-    glMatrixMode( GL_MODELVIEW );
-    glLoadIdentity();
-    glTranslatef( x, y, 0 );
-    glScalef( xRadius / radius, yRadius / radius, 1.0 );    // by ratio of major to minor axes
-    GLUquadricObj *disk = gluNewQuadric();
-    gluDisk( disk, radius - 4, radius, int( radius ), 1 );
-    gluDeleteQuadric( disk );
-    glLoadIdentity();
-    glFlush();
-}
-
-/******************************************************************************/
-
-// draw an ellipse of given radii at a specified position using the given RGB color value
-void DrawFilledEllipse( float xRadius, float yRadius, int x, int y, const float color[] )
-{
-    float radius = xRadius < yRadius ? xRadius : yRadius;   // stretch circle into ellipse
-    glColor3fv( color );
-    glMatrixMode( GL_MODELVIEW );
-    glLoadIdentity();
-    glTranslatef( x, y, 0 );
-    glScalef( xRadius / radius, yRadius / radius, 1.0 );    // by ratio of major to minor axes
-    GLUquadricObj *disk = gluNewQuadric();
-    gluDisk( disk, 0, radius, int( radius ), 1 );
-    gluDeleteQuadric( disk );
-    glLoadIdentity();
-    glFlush();
-}
-
-/******************************************************************************/
-
-// write a text string at a specified position using the given RGB color value
-void DrawBitmapString( const char *string, float x, float y, const float color[] )
-{
-    glColor3fv( color );
-    glRasterPos2f( x, y );
-
-    // glutBitmapString is available in freeglut
-    glutBitmapString( GLUT_BITMAP_TIMES_ROMAN_24, (const unsigned char *)string );
-
-    // GLUT only supports glutBitmapCharacter
-    // while ( *string ) glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, *string++ );
-}
-
-void DrawStrokeString( const char *string, float x, float y, const float color[] )
-{
-    glColor3fv( color );
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glTranslatef( x, y, 0 );
-    // while ( *string ) glutStrokeCharacter( GLUT_STROKE_ROMAN, *string++ );
-    glutStrokeString( GLUT_STROKE_ROMAN, (const unsigned char *)string );
-    glPopMatrix();
-}
-
